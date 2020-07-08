7ef9254bbab28f46e0b0dd648027338416074e16
h3isenbug <h.kalantari.1997@gmail.com>
2020-07-08 20:32:57 +0430

    added url viewed event

--- pkg/messages/analytics.go
+++ pkg/messages/analytics.go
@@ -1,38 +1,7 @@
 package messages
 
-type baseAnalyticsQuery struct {
-	BaseMessage
-
-	Period   string `json:"period"` //today, past-1-day, past-7-days, past-30-days
-	ShortURL string `json:"shortURL"`
-}
-
-type URLOverallAnalyticsQuery struct {
-	baseAnalyticsQuery
-}
-
-type URLPerUniqueUserAnalyticsQuery struct {
-	baseAnalyticsQuery
-}
-
-type baseAnalyticsReadyEvent struct {
-	BaseMessage
-
-	Period   string `json:"period"` //today, past-1-day, past-7-days, past-30-days
-	ShortURL string `json:"shortURL"`
-
-	OverallVisits       uint64 `json:"overallVisits"`
-	MobileVisits        uint64 `json:"mobileVisits"`
-	DesktopVisits       uint64 `json:"desktopVisits"`
-	OtherPlatformVisits uint64 `json:"otherPlatformVisits"`
-
-	BrowserVisits map[string]uint64 `json:"browserVisits"`
-}
-
-type URLOverallAnalyticsReadyEvent struct {
-	baseAnalyticsQuery
-}
-
-type URLPerUniqueUserAnalyticsReadyEvent struct {
-	baseAnalyticsQuery
+type URLViewedEvent struct {
+	ShortPath string `json:"shortPath"`
+	ETag      string `json:"etag"`
+	UserAgent string `json:"userAgent"`
 }
