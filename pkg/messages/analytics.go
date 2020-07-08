b959d70e574139303fc25f5073fdb25b9e38aa78
h3isenbug <h.kalantari.1997@gmail.com>
2020-07-08 20:38:53 +0430

    added missing type field

--- pkg/messages/analytics.go
+++ pkg/messages/analytics.go
@@ -1,6 +1,7 @@
 package messages
 
 type URLViewedEvent struct {
+	Type      string `json:"type"`
 	ShortPath string `json:"shortPath"`
 	ETag      string `json:"etag"`
 	UserAgent string `json:"userAgent"`
